e97a4f54ce941225854e9278eec5b4d388f8a73f
Yolo <yolo@fakeemail.com>
2022-08-18 14:12:34 -0600

    Finish browser only functionality

--- javascript.js
+++ javascript.js
@@ -24,12 +24,12 @@ function operate(operator,num1,num2){
         answer = multiply(num1,num2);
     } else if (operator === "/") {
         answer = divide(num1,num2);
-    } else if (operator === "=") {
-        answer = num1;
-        console.log("Operator =");
     } else {
         answer = "Error-Operate";
     }
+    // Round to a specific number of significant digits
+    // https://www.w3schools.com/jsref/jsref_toprecision.asp
+    answer = answer.toPrecision(8);
     return answer;
 }
 
@@ -41,19 +41,23 @@ allButtons.forEach(item => {item.addEventListener('click', calculator)});
 const display = document.getElementById("display");
 display.innerText = 0;
 
-let num1 = undefined;
-let num2 = undefined;
+let num1 = 0;
+let num2 = 0;
 let numString = '';
 let op =  undefined;
 
 function resetAll(){
-    num1 = undefined;
-    num2 = undefined;
+    num1 = 0;
+    num2 = 0;
     numString = '';
     op = undefined;
 }
 
-
+// how to display unique characters
+// https://www.fileformat.info/info/unicode/char/23fb/index.htm
+function divZeroError() {
+    display.innerText = '2 \u221E & B ' + `\u23FB D`;
+}
 
 function btnChoice(e){
     let input = e.target.id;
@@ -91,7 +95,7 @@ function calculator(e){
     } else if (Number.isInteger(x)) {
         numString += `${x}`;
         display.innerText = numString;
-        console.log(`extend string with ${x}`);
+        //console.log(`extend string with ${x}`);
     } else {
         switch (x){
             case '.':
@@ -106,87 +110,93 @@ function calculator(e){
                 break;
             case 'Sign':
                 if (numString === '0' || (numString === '' && num1 === 0)) {
-                    console.log('Sign: do nothing');
+                    //console.log('Sign: do nothing');
                 } else if (/\-/.test(numString)){
                     numString = numString.slice(1);
                     display.innerText = numString;
-                    console.log('Sign: sign remove');
+                    //console.log('Sign: sign remove');
                 } else if (numString !== '') {
                     numString = '-' + numString;
                     display.innerText = numString;
-                    console.log('Sign: sign add');
+                    //console.log('Sign: sign add');
                 } else {
                     // Sign change on previously calculated answer
                     num1 *= -1;
                     display.innerText = num1;
-                    console.log('Sign: else');
+                    //console.log('Sign: else');
                 }
                 break;
             case 'Delete':
                 if (numString === '' || numString === '0') {
-                    console.log('Delete: no number');
+                    //console.log('Delete: no number');
                 } else if (/\-/.test(numString)) {
                     if (numString.length > 2){
                         numString = numString.slice(0,-1);
                         display.innerText = numString;
-                        console.log('Delete (-): > 2 length');
+                        //console.log('Delete (-): > 2 length');
                     } else {
                         numString = '';
                         display.innerText = '0';
-                        console.log('Delete (-): <= 2 length');
+                        //console.log('Delete (-): <= 2 length');
                     }
                 } else {
                     if (numString.length > 1) {
                         numString = numString.slice(0,-1);
                         display.innerText = numString;
-                        console.log('Delete (+): > 1 length');
+                        //console.log('Delete (+): > 1 length');
                     } else {
                         numString = '';
                         display.innerText = '0';
-                        console.log('Delete (+): <= 1 length');
+                        //console.log('Delete (+): <= 1 length');
                     }
                 }
                 break;
             case 'Clear':
                 resetAll();
                 display.innerText = '0';
-                console.log('Clear');
+                //console.log('Clear');
                 break;
             case '=':
                 if (op === undefined) {
-                    console.log('= 01');
+                    //console.log('= 01');
                     if (numString !== '') {
                         // nothing
-                        console.log('no op yet');
+                        //console.log('no op yet');
                     }
                 } else if (op !== undefined) {
-                    console.log('= 02');
+                    //console.log('= 02');
                     
                     if (numString === '') {
                         num2 = num1;
-                        console.log('no numString');
+                        //console.log('no numString');
                     } else {
                         num2 = parseFloat(numString);
-                        console.log('yes numString');
+                        //console.log('yes numString');
                     }
-                    console.log(`Before = op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+                    //console.log(`Before = op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
                     num1 = operate(op,num1,num2);
-                    display.innerText = num1;
                     
-                    numString = `${num1}`;
+                    // Catches divide by zero
+                    if (op === '/' && num2 === 0) {
+                        divZeroError();
+                        numString = '';
+                        //console.log('Divide by zero after = operation');
+                    } else {
+                        display.innerText = num1;
+                        numString = `${num1}`;
+                        //console.log('Normal solution after = operation');
+                    }
+                    
                     op = undefined;
-                    num1 = undefined;
-                    num2 = undefined;
-                    console.log(`After = op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+                    num1 = 0;
+                    num2 = 0;
+                    //console.log(`After = op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
                 } else {
-                    console.log('Error in = code');
+                    //console.log('Error in = code');
                 }
                 break;
-                
-            // Determine desired functionality of Divide by Zero
-            // place same error into = code (or a function that can work in both)
+
             // Limit the length of result calculated
-            // Basic funtionality seems to work
             case '+':
             case '*':
             case '/':
@@ -197,80 +207,61 @@ function calculator(e){
                         numString = '0';
                     }
                     op = x;
-                    console.log(`sign defined as ${op}`);
+                    //console.log(`sign defined as ${op}`);
                     num1 = parseFloat(numString);
                     numString = '';
                 } else {
-                    console.log(`01 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+                    //console.log(`01 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
                     if (numString === '') {
                         num2 = num1;
-                        console.log('A');
+                        //console.log('A');
                     }
                     else {
                         num2 = parseFloat(numString);
-                        console.log('B');
+                        //console.log('B');
                     }
-                    console.log(`02 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+                    //console.log(`02 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
                     num1 = operate(op,num1,num2);
-                    console.log(`03 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                    
-                    // SHOULD be copied in = section ***** MAKE SURE TO UPDATE
-                    // divide by zero error
+                    //console.log(`03 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+
                     // Catches dividing by zero and the edge case of num1 = NaN
-                    if ((x === '/' && Number.isNaN(num1)) || Number.isNaN(num1) || num1 === Infinity) {
-                        display.innerText = 'Divided by Zero';
-                        console.log('Divide by zero');
+                    if (x === '/' && num2 === 0) {
+                        divZeroError();
+                        //console.log('Divide by zero');
                         num1 = 0;
                         num2 = 0;
                         resetAll();
                     } else {
-                        console.log('Not divide by zero');
+                        //console.log('Not divide by zero');
                         display.innerText = num1;
                         op = x; // Allows stringing together multiple operators without using "="
                     }
                     
                     numString = '';
-                    console.log(`Next sign: ${op}`);
-                    console.log(`04 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
+                    //console.log(`Next sign: ${op}`);
+                    //console.log(`04 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
                 }
                 break;
         }
     }
 }
 
-/*
-                // op being undefined/defined determines whether the calculation goes through
-                if (op === undefined) {
-                    if (numString === '') {
-                        numString = '0';
-                        console.log('set numString to 0');
-                    }
-                    op = x;
-                    num1 = parseFloat(numString);
-                    
-                    console.log(`Setting up 01 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                    num2 = num1;
-                    numString = '';
-                    console.log(`Setting up 02 op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                } else {
-                    if (num1 === undefined) {num1 = 0;}
-                    if (num2 === undefined) {num2 = 0;}
-                    if (numString === '') {
-                        numString = '0';
-                        console.log('set numString to 0');
-                    }
-                    op = x;
-                    num2 = parseFloat(numString);
-                    console.log(`Before Calc op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                    
-                    num1 = operate(op,num1,num2);
-                    display.innerText = num1;
-                    numString = `${num1}`;
-                    console.log(`After Calc op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                    
-                    op = '';
-                    numString = '';
-                    console.log(`Reset Calc op: ${op} num1: ${num1} num2: ${num2} numString: ${numString}`);
-                }
-                break;
-*/
+// Detecting keyboard buttons in javascript
+// https://stackoverflow.com/questions/16089421/how-do-i-detect-keypresses-in-javascript
+// https://www.techcoil.com/blog/how-to-detect-keyboard-presses-made-to-the-browser-screen-with-javascript/
+// https://howtodoinjava.com/jquery/jquery-difference-between-keypress-and-keydown-events/
+document.addEventListener('keydown', onKeyPressed);
+
+function onKeyPressed(e) {
+    var keyCode = e.keyCode;
+    var key = e.key;
+    // Lets you see the code livetime
+    console.log(`Code: ${keyCode} Key: ${key}`);
+}
+
+document.addEventListener('keypress', detectKeyboardNumbers);
+function detectKeyboardNumbers(e){
+    var keyCode = e.keyCode;
+    var key = e.key;
+    
+}
